1035283be9b42228997ffc7f0f9a81877ca30f48
Gustavo Arias Rojas <tavoarias@gmail.com>
2017-05-26 15:38:38 -0600

    Inicio de programación del sistema

---
+++ DecisionSupportSystem/DecisionSupportSystem/frm_login.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace DecisionSupportSystem
+{
+    partial class frm_login
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frm_login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(514, 256);
+            this.Name = "frm_login";
+            this.Text = "DSS - Bienvenido al sistema";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
